ba62eaf4a1b1e56114bcf782727d74a7e48609a8
nayutalienx <nayutalienx@gmail.com>
2019-11-24 18:51:33 +0300

    dal and contracts added

---
+++ CloudJ/DataAccessLayer/DataAccessLayer.Abstraction/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAccessLayer.Abstraction
+{
+    public class Class1
+    {
+    }
+}
---
+++ CloudJ/DataAccessLayer/DataAccessLayer.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAccessLayer.Models
+{
+    public class Class1
+    {
+    }
+}
