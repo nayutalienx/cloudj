3c1dcea72fbd334dc353f8327c34f4ca2129b7ec
nayutalienx <nayutalienx@gmail.com>
2019-11-29 04:11:44 +0300

    simple html pages in process

---
+++ CloudJ/CloudJ.Client/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using CloudJ.Client.Clients;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudJ.Client.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ISolutionApiClient _solutionApiClient;
+        public ProductController(ISolutionApiClient solutionApiClient)
+        {
+            _solutionApiClient = solutionApiClient;
+        }
+
+        /// <summary>
+        /// Страница конкретного решения
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:long}")]
+        public async Task<IActionResult> GetProductById(long id)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Добавить ссылку к решению
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:long}/links")]
+        public async Task<IActionResult> AddLink(long id)
+        {
+            return View();
+        }
+    }
+}
